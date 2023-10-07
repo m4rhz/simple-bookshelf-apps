6de6b0b6b66343f583e337dad1007e0b30bd199d
Aqil <139866147+m4rhz@users.noreply.github.com>
2023-10-08 03:05:46 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const submitForm = document.getElementById("bookSubmit");
+    submitForm.addEventListener("click", function(event) {
+        event.preventDefault();
+        tambahBuku();
+        hapusForm();
+    });
 
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
+});
+
+document.addEventListener("ondatasaved", () => {
+    console.log("Data berhasil disimpan");
+});
+
+document.addEventListener("ondataloaded", () => {
+    refreshDataFromList();
+});
+
+const checkType = document.getElementById("inputBookIsComplete");
+checkType.addEventListener("click", () => {
+    if (checkType.checked) {
+        document.getElementById("tipeBuku").innerHTML = "<strong>Sudah Selesai Dibaca</strong>";
+    } else {
+        document.getElementById("tipeBuku").innerHTML = "<strong>Belum Selesai Dibaca</strong>";
+    }
+})
